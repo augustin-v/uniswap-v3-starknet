5928999bdc1898e39aa6186aac65ba953b5b861f
Jaja <jajak5242@gmail.com>
2025-05-31 13:19:37 +0100

    feat: Add Starknet wallet integration into the frontend with a connect button for a user-friendly interface.

---
+++ ui/Component/starknet-wallet/ConnectWallet.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useConnect, useAccount, useDisconnect, Connector } from "@starknet-react/core";
+
+import { StarknetkitConnector, useStarknetkitConnectModal } from 'starknetkit';
+
+export default function ConnectWallet() {
+  const { connect, connectors } = useConnect();
+  const { address } = useAccount();
+  const { disconnect } = useDisconnect();
+
+   const { starknetkitConnectModal } = useStarknetkitConnectModal({
+    connectors: connectors as StarknetkitConnector[],
+  });
+
+  async function connectWallet() {
+    const { connector } = await starknetkitConnectModal();
+    if (!connector) {
+      return;
+    }
+    await connect({ connector: connector as Connector });
+  } 
+  if (!address) {
+    return (
+      <button
+        onClick={connectWallet}
+        className=" text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-colors p-4"
+      >
+        Connect Wallet
+      </button>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="p-2 bg-gray-100 rounded-lg ">
+        Connected: {address?.slice(0, 6)}...{address?.slice(-4)}
+      </div>
+      <button
+        onClick={() => disconnect()}
+        className="px-4 py-2 text-white bg-red-600 rounded-lg hover:bg-red-700 transition-colors"
+      >
+        Disconnect
+      </button>
+    </div>
+  );
+}
